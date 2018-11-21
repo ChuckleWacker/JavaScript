b8f4dd9764501ab2650d803755cbc5f9276f3286
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-11-21 11:04:31 -0800

    Update Unit2_Conditionals_&_Functions.js

--- Unit2_Conditionals_&_Functions.js
+++ Unit2_Conditionals_&_Functions.js
@@ -1,1 +1,24 @@
-//
+// If Statement
+let sale = true; //If set to true, the condition below prints to screen.
+if (sale) {
+  console.log("Time to buy!");
+}
+
+
+//Else Statement
+let sale = true;
+sale = false;
+if (sale) {
+  console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale.');
+}
+
+
+//Else Statement with Comparison Operator
+let hungerLevel = 7;
+if (hungerLevel > 7) {
+  console.log("Time to eat!");
+} else {
+  console.log("We can eat later!");
+}
