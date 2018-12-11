bf73c0e5a9ab39bceeb125c20ff5885fe1767fd6
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-12-11 00:40:49 -0800

    Update Unit3_Code_Challenges.js

--- Unit3_Code_Challenges.js
+++ Unit3_Code_Challenges.js
@@ -81,3 +81,15 @@ function reportingForDuty(rank, lastName) {
   return `${rank} ${lastName} reporting for duty!`;
 }
 console.log(reportingForDuty('Private', 'Fido')) // Should return 'Private Fido reporting for duty!'
+
+
+//Roll Dice function
+const rollTheDice = () => {
+  // Math.random() gives us a random number from 0 up to, but not including, 1
+  // We multiplied that by 6 to get a number between 0 and up to, but not including, 6
+  // But since we actually wanted numbers from 1 to 6, inclusive, we added 1
+    let die1 = (Math.random()) * 6 + 1
+    let die2 = (Math.random()) * 6 + 1
+    return die1 + die2
+}
+console.log(rollTheDice())
