c0903663367c57e4ddf180068cf3c8a652e1d775
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-12-11 01:11:39 -0800

    Update Unit3_Code_Challenges.js

--- Unit3_Code_Challenges.js
+++ Unit3_Code_Challenges.js
@@ -92,4 +92,24 @@ const rollTheDice = () => {
     let die2 = (Math.random()) * 6 + 1
     return die1 + die2
 }
-console.log(rollTheDice())
+console.log(rollTheDice()) //Print random number
+
+
+//Calculate Weight By Planet function
+function calculateWeight(earthWeight, planet) {
+  switch (planet) {
+    case "Mercury":
+      return earthWeight * 0.378;
+    case "Venus":
+      return earthWeight * 0.907;
+    case "Mars":
+      return earthWeight * 0.377;
+    case "Jupiter":
+      return earthWeight * 2.36;
+    case "Saturn":
+      return earthWeight * 0.916;
+    default:
+      return "Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.";
+  }
+}
+console.log(calculateWeight(100, 'Jupiter')) // Print 236
