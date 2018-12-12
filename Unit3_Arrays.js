523287d4d5d49fe2496a2b53708998cef22d69ba
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-12-11 23:50:34 -0800

    Create Unit3_Arrays.js

---
+++ Unit3_Arrays.js
@@ -0,0 +1,15 @@
+//Define and Print array
+let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
+console.log(newYearsResolutions);
+
+//Define and Print array
+const hobbies = ["gaming", "coding", "travel"];
+console.log(hobbies);
+
+//Accessing Elements in Array
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+let listItem = famousSayings[0];
+console.log(listItem); //Print "Fortune favor the brave"
+console.log(famousSayings[2]) //Print "Where there is love there is life"
+console.log(famousSayings[3]) //undefined as tehre is nothing in this spot in the array
+
