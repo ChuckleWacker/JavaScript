df3b42c3f45cd25725188114015b40938eba1b7e
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-12-12 00:01:40 -0800

    Update Unit3_Arrays.js

--- Unit3_Arrays.js
+++ Unit3_Arrays.js
@@ -2,9 +2,11 @@
 let newYearsResolutions = ['Keep a journal', 'Take a falconry class', 'Learn to juggle'];
 console.log(newYearsResolutions);
 
+
 //Define and Print array
 const hobbies = ["gaming", "coding", "travel"];
 console.log(hobbies);
+
 
 //Accessing Elements in Array
 const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
@@ -12,4 +14,11 @@ let listItem = famousSayings[0];
 console.log(listItem); //Print "Fortune favor the brave"
 console.log(famousSayings[2]) //Print "Where there is love there is life"
 console.log(famousSayings[3]) //undefined as tehre is nothing in this spot in the array
+
+
+//Update specific items in array
+let groceryList = ['bread', 'tomatoes', 'milk'];
+groceryList[1] = "avocados";
+console.log(groceryList); //Prints "avocados as second array item
+
 
