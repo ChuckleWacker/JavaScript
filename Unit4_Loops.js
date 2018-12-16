b60774a3aac2924422053c8f660a9dd5b50dc2f8
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-12-16 13:35:38 -0800

    Update Unit4_Loops.js

--- Unit4_Loops.js
+++ Unit4_Loops.js
@@ -4,3 +4,15 @@ let socialMedia = ['Instagram', 'Facebook', 'Twitter'];
 for (let socialMediaIndex = 0; socialMediaIndex < socialMedia.length; socialMediaIndex++) {
 	console.log(socialMedia[socialMediaIndex]);
 }
+
+
+//LOOP prints "c 1 2 3 a 1 2 3 t 1 2 3"
+//Each time the outer loop runs, the inner loop completes all iterations.
+const animal = 'cat';
+
+for (let i = 0; i < animal.length; i++) {
+  console.log(animal[i]);
+  for (let j = 1; j < 4; j++) {
+    console.log(j);
+  }
+}
