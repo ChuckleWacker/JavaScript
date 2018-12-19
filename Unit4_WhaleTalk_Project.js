9544dfcd85855ec2faa7934dcec9a2d3ec01973f
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-12-18 23:04:10 -0800

    Update Unit4_WhaleTalk_Project.js

--- Unit4_WhaleTalk_Project.js
+++ Unit4_WhaleTalk_Project.js
@@ -1,1 +1,26 @@
+//WHALE TALK Translator
 
+// variable to translate
+let input = "turpentine and turtles";
+
+// array of vowels to be extracted from input
+const vowels = ["a", "e", "i", "o", "u"];
+
+// array to store vowels from input string
+let resultArray = [];
+
+// for loop to compare the text of input variable to vowels array
+for (i = 0; i < input.length; i++) {
+  for (j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      resultArray.push(input[i]);
+    	if (input[i] === "e" || input[i] === "u") {
+        resultArray.push(input[i]);
+      }
+    }
+  }
+  //console.log("i is " + i); //count iterations done to verify for loop works
+}
+
+// Print resultArray joined and all UpperCase
+console.log(resultArray.join("").toUpperCase());  // Should print "UUEEIEEAUUEE"
