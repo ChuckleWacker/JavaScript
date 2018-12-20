e4529bf5106124566e701b39b45c8182f35c71ee
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-12-20 01:26:17 -0800

    Update Unit4_HigherOrderFunction.js

--- Unit4_HigherOrderFunction.js
+++ Unit4_HigherOrderFunction.js
@@ -1,1 +1,11 @@
+//A higher-order function is a function that either accepts functions as parameters, returns a function, or both!
+
+//MAKE A SHORTER NAME (functions as Data)
+// Assign long named function as the variable to a new function which is shorter in name
+const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+// invoke new function
+is2p2(); 
+// .name property helps us determine what function we assigned to is2p2, which is "checkThatTwoPlusTwoEqualsFourAMillionTimes"
+console.log(is2p2.name); 
+
 
