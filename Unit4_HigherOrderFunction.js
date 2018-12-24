a6e8c76bd2793cb237025f1c168ce3abb4b0aa6b
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-12-23 21:20:25 -0800

    Update Unit4_HigherOrderFunction.js

--- Unit4_HigherOrderFunction.js
+++ Unit4_HigherOrderFunction.js
@@ -9,3 +9,37 @@ is2p2();
 console.log(is2p2.name); 
 
 
+
+
+/EXAMPLE of creating a higher order function and calling it within a new function to check things like...
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for(let i = 1; i <= 1000000; i++) {
+    if ( (2 + 2) != 4) {
+      console.log('Something has gone very wrong :( ');
+    }
+  }
+};
+
+const addTwo = num => num + 2;
+// checks the time it took for the function to run, using this higher order function
+const timeFuncRuntime = funcParameter => {
+  let t1 = Date.now();
+  funcParameter();
+  let t2 = Date.now();
+  return t2 - t1;
+};
+
+// Write your code below
+let time2p2 = timeFuncRuntime(checkThatTwoPlusTwoEqualsFourAMillionTimes);
+
+function checkConsistentOutput(func, val) {
+  let firstTry = func(val);
+  let secondTry = func(val);
+  if (firstTry === secondTry) {
+    return firstTry;
+  } else {
+    return "This function returned inconsistent results";
+  }
+};
+
+console.log(checkConsistentOutput(addTwo, 1));
