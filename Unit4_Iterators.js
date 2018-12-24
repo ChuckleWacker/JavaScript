e86f94c6c2a7468b9bec3af2b6bc4fbdbf6b3356
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-12-24 13:30:40 -0800

    Update Unit4_Iterators.js

--- Unit4_Iterators.js
+++ Unit4_Iterators.js
@@ -16,3 +16,20 @@ const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
 const onlyNumbers = things.filter(thing => {
   return typeof thing === 'number';
 });
+
+
+//FOREACH try using the arrow function, option #2
+//FOREACH option #1
+groceries.forEach(function(groceryItem) {
+  console.log(" - " + groceryItem);
+});
+
+//FOREACH option #2 - We can pass a callback for .forEach() is to use arrow function syntax.
+groceries.forEach(groceryItem => console.log(groceryItem));
+
+//FOREACH option #3 - We can also define a function beforehand to be used as the callback function.
+function printGrocery(element){
+  console.log(element);
+}
+groceries.forEach(printGrocery);
+
