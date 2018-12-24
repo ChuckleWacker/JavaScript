886d97fdc65748b5a34daf98070075244e13e69a
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-12-23 22:00:24 -0800

    Update Unit4_Iterators.js

--- Unit4_Iterators.js
+++ Unit4_Iterators.js
@@ -1,1 +1,18 @@
+//FOR EACH method
+const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
+artists.forEach(artist => {
+  console.log(artist + ' is one of my favorite artists.');
+});
 
+//MAP Method
+const numbers = [1, 2, 3, 4, 5];
+const squareNumbers = numbers.map(number => {
+  return number * number;
+});
+console.log(squareNumbers);
+
+//FILTER Method
+const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+const onlyNumbers = things.filter(thing => {
+  return typeof thing === 'number';
+});
