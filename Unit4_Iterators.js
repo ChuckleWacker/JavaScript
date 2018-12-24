ffe6f39daf0596343f41d8de77431f5dc3090510
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-12-24 14:49:34 -0800

    Update Unit4_Iterators.js

--- Unit4_Iterators.js
+++ Unit4_Iterators.js
@@ -1,21 +1,82 @@
-//FOR EACH method
+//FOREACH method, does function to each item in current array
 const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
 artists.forEach(artist => {
   console.log(artist + ' is one of my favorite artists.');
 });
 
-//MAP Method
+//MAP method, creates new array usig old array and function
 const numbers = [1, 2, 3, 4, 5];
 const squareNumbers = numbers.map(number => {
   return number * number;
 });
 console.log(squareNumbers);
 
-//FILTER Method
+//FILTER method, creates new array by filtering out items based on function
 const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
 const onlyNumbers = things.filter(thing => {
   return typeof thing === 'number';
 });
+
+//FINDINDEX method, returns first element that evaluates as true, if nothing is found then returns -1
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+const foundAnimal = animals.findIndex(animal => {
+  return animal == "elephant";
+})
+console.log(foundAnimal); // 7
+console.log(animals[foundAnimal]); // elephant
+
+const startsWithS = animals.findIndex(animal => {
+  return animal[0] == "s";
+})
+console.log(startsWithS); // 3
+console.log(animals[startsWithS]); // seal 
+
+//REDUCE method, returns a single value after iterating through the elements of an array.
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ' + accumulator);
+  console.log('The value of currentValue: ' + currentValue);
+  return accumulator + currentValue;
+}, 10)
+console.log(newSum); // Prints 16, or 26 if we added 10 as the second argument to set a start number
+
+//SOME & EVERY methods, check if there are "some or everthing" of something
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+// SOME
+console.log(words.some((word) => { return word.length < 6 })); // True
+// Use filter to create a new array of words with more than 6 characters
+const interestingWords = words.filter(word => { return word.length > 5 });
+// EVERY
+console.log(interestingWords.every((word) => { return word.length > 5 } )); // True
+
+
+// ************************************************************* //
+
+//MORE EXAMPLES:
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  forEach method that will return undefined
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
+
+// filter method that will return a new array
+const longCities = cities.filter(city => city.length > 7);
+
+// reduce method that will return a single value
+const word = cities.reduce((acc, currVal) => {
+  return acc + currVal[0]
+}, "C");
+console.log(word)
+
+// map method that will return a new array
+const smallerNums = nums.map(num => num - 5);
+
+// every (or some) method that will return a boolean value
+nums.every(num => num < 0);
+
+
+// ************************************************************* //
 
 
 //FOREACH Examples: use the arrow function, option #2
