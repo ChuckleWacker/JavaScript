a1f4632401bde7a3801bbd3ebf30ed9337d51ab2
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-12-25 19:58:18 -0800

    Update Unit6_Challenges.js

--- Unit6_Challenges.js
+++ Unit6_Challenges.js
@@ -64,3 +64,112 @@ const acceptEverything = arr => {
 }
 
 
+// MAP array, square the number from the array 
+const numbers = [2, 7, 9, 171, 52, 33, 14];
+const toSquare = num => num * num
+const squareNums = arr => arr.map(toSquare) 
+
+
+// Function within a Function
+const shoutGreetings = arr => arr.map(word => word.toUpperCase() + '!');
+const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo']
+console.log(shoutGreetings(greetings))
+
+
+// Function takes an array and sorts in descending order
+const sortYears = arr => arr.sort((x, y) => y - x);
+const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922];
+console.log(sortYears(years));
+
+
+// 
+const justCoolStuff = (firstArray, secondArray) => firstArray.filter(item => secondArray.includes(item))
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+console.log(justCoolStuff(myStuff, coolStuff))
+
+
+//
+const isTheDinnerVegan = arr => arr.every(food => food.source === 'plant');
+const dinner = [{name: 'hamburger', source: 'meat'}, {name: 'cheese', source: 'dairy'}, {name: 'ketchup', source:'plant'}, {name: 'bun', source: 'plant'}, {name: 'dessert twinkies', source:'unknown'}];
+console.log(isTheDinnerVegan(dinner))
+
+
+//
+const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
+const sortSpeciesByTeeth = arr => arr.sort((speciesObj1, speciesObj2) => speciesObj1.numTeeth > speciesObj2.numTeeth)
+console.log(sortSpeciesByTeeth(speciesArray))
+
+
+//
+const findMyKeys = arr => arr.findIndex(item => item === 'keys')
+const randomStuff = ['credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'plastic spoon'];
+console.log(findMyKeys(randomStuff))
+
+
+//
+const dogFactory = (name, breed, weight) => {
+    return {
+        _name: name,
+        _breed: breed,
+        _weight: weight,
+        get name() {
+            return this._name;
+        },
+        set name(newName) {
+            this._name = newName;
+        },
+        get breed() {
+            return this._breed;
+        },
+        set breed(newBreed) {
+            this._breed = newBreed;
+        },
+        get weight() {
+            return this._weight;
+        },
+        set weight(newWeight) {
+            this._weight = newWeight;
+        },
+        bark() {
+            return 'ruff! ruff!'
+        },
+        eatTooManyTreats() {
+            this._weight++
+        }
+    }
+}
+// Solution to checkpoint 1:
+const dogFactory = (name, breed, weight) => {
+      return {
+            name: name,
+            breed: breed,
+            weight: weight
+      }
+}
+// Solution to checkpoint 2:
+const dogFactory = (name, breed, weight) => {
+      return {
+            _name: name,
+            _breed: breed,
+            _weight: weight,
+            get name() {
+                  return this._name;
+            },
+            set name(newName) {
+                  this._name = newName;
+            },
+            get breed() {
+                  return this._breed;
+            },
+            set breed(newBreed) {
+                  this._breed = newBreed;
+            },
+            get weight() {
+                  return this._weight;
+            },
+            set weight(newWeight) {
+                  this._weight = newWeight;
+            }
+      }
+}
