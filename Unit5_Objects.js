ef917f610e8abba09628f2178f7d7c0977e76936
ChuckleWacker <daniel.a.boggs@gmail.com>
2018-12-25 20:17:43 -0800

    Update Unit5_Objects.js

--- Unit5_Objects.js
+++ Unit5_Objects.js
@@ -1,1 +1,17 @@
+//Starter Objects
+// Object A
+let spaceship = {
+  homePlanet: "Earth",
+  "Fuel Type": "diesel",
+  color: "silver",
+  numCrew: 5,
+  flightPath: ["Venus", "Mars", "Saturn"]
+};
+// Object B
+let fasterShip = {
+  "Fuel Type": "Turbo Fuel",
+  color: "silver"
+};
+
+
 
